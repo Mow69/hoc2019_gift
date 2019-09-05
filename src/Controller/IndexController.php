a4b2c26584bd4e9f14b26e339054870a89640013
Mouaz S <mouazmouaz@hotmail.fr>
2019-09-05 16:47:15 +0200

    Button added

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
@@ -22,10 +23,19 @@ class IndexController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact()
+    public function contact(Request $request)
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            // TODO Rediriger vers une page de confirmation
+        }
 
         return $this->render(
             'index/contact.html.twig',
