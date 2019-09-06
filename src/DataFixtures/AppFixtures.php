6ae11a653207f7ab0beaf5b70cfba3b149ca4b72
Mouaz S <mouazmouaz@hotmail.fr>
2019-09-06 17:00:34 +0200

    GiftAuthenticator added

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,49 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Gift;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    public $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;    
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('bob@bob.com')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($this->encoder->encodePassword(
+            $user, 
+            'blablabla'
+        ));
 
+    $manager->persist($user);
+    $manager->flush();
+
+
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 10; $i++) {
+        // Je crée un objet
+        $gift = new Gift();
+        // J'affect ses attributs
+        $gift->setTitle($faker->sentence(5, true))
+            ->setDescription($faker->paragraph(5, true));
+        // J'indique à mon gestionnaire d'entités que je veux insérer cet objet en BDD
+        $manager->persist($gift);
+    }
+
+
+        // Je valide les modifications effectuées (insertions, modifications sur des enregistrements, etc...) dans la BDD
         $manager->flush();
     }
 }
